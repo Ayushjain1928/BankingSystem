b8d3223f5b5d4c26227c2d7dc666073b013f0944
Frost21-max <bishtpiyush363@gmail.com>
2025-11-18 13:35:40 +0000

    changed by piyush

--- src/account_creation.c
+++ src/account_creation.c
@@ -2,34 +2,24 @@
 #include <mysql/mysql.h>
 #include <termios.h>
 #include <unistd.h>
+#include <ctype.h>
 
 #define PASSWD "Mydatabases@123"
 #define USER "root"
-typedef enum
-{
-    MALE = 0,
-    FEMALE = 1,
-    OTHER = 3
-} Gender;
-typedef enum
-{
-    SAVINGS = 0,
-    CURRENT = 1
-} Account_type;
 
 typedef struct
 {
     unsigned long long account_no;
-    char name;
+    char name[20];
     unsigned int age;
-    Gender gender;
+    char gender[10];
     char date_of_birth[11];
     char aadhar_no[18];
     char pan_no[18];
     char phone[15];
     char email[20];
     unsigned long long balance;
-    Account_type type;
+    char account_type[10];
 } Account;
 
 void mysql_query_excuter(const char *, const char *); // function prototype for connection
@@ -76,43 +66,26 @@ Account acc;
 int user_menu()
 {
     printf("Write down your name.");
-    scanf("%s", acc.name);
+    scanf("%s", &acc.name);
     printf("Write down your age.");
-    scanf("%d", acc.age);
+    scanf("%u", &acc.age);
     printf("Write down your Date of Birth(YYYY-MM-DD).");
-    scanf("%10s", acc.date_of_birth);
+    scanf("%10s", &acc.date_of_birth);
     printf("Write down your Gender(Male=0,Female=1,Other=Any Number).");
-    scanf("%d", acc.gender);
-    Gender g;
-    switch (g)
-    {
-    case 0:
-        return "Male";
-    case 1:
-        return "Female";
-    default:
-        return "Other";
-    }
+    scanf("%d", (int *)&acc.gender);
+    
+   
     printf("Write down your Aadhar Number.");
-    scanf("%s", acc.aadhar_no);
+    scanf("%s", &acc.aadhar_no);
     printf("Write down your Pan Number.");
-    scanf("%s", acc.pan_no);
+    scanf("%s", &acc.pan_no);
     printf("Write down your Phone Number.");
-    scanf("%s", acc.phone);
+    scanf("%s", &acc.phone);
     printf("Write down your age.");
-    scanf("%s", acc.email);
+    scanf("%s", &acc.email);
     printf("Write down your Type of Account(Savings=0,Current=1).");
-    scanf("%s", acc.type);
-    Account_type t;
-    switch (t)
-    {
-    case 0:
-        return "Savings";
-    case 1:
-        return "Current";
-    default:
-        return "Savings";
-    }
+    scanf("%s", &acc.account_type);
+    
 
     char query[1024];
 
@@ -129,7 +102,7 @@ int user_menu()
             acc.phone,
             acc.email,
             acc.balance,
-            acc.type);
+            acc.account_type);
 
     mysql_query_excuter(query, "account_information");
 
