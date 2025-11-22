75194f154e08fd91121db55834484f1a6202978d
Frost21-max <bishtpiyush363@gmail.com>
2025-11-22 07:10:33 +0000

    Changed by Piyush

--- src/account_management.c
+++ src/account_management.c
@@ -5,45 +5,14 @@
 
 #define PASSWD "Mydatabases@123"
 #define USER "root"
-void mysql_value_fetcher(const char* query , const char* databases);
-void mysql_value_fetcher(const char* query , const char* databases){
-    
-}
-void mysql_query_excuter(const char *query ,const char *databases ) {
-    MYSQL *conn = mysql_init(NULL);
-
-    // Check initialization
-    if (conn == NULL) {
-        printf("mysql_init failed\n");
-        return;
-    }
-
-    // Connect to server
-    if (!mysql_real_connect(conn, "localhost", USER, PASSWD,databases, 0, NULL, 0)) {
-        printf("Connection failed: %s\n", mysql_error(conn));
-        mysql_close(conn);
-        return;
-    }
-
-    // Execute query
-    if (mysql_query(conn, query)) {
-        printf("Query execution failed: %s\n", mysql_error(conn));
-        mysql_close(conn);
-        return;
-    }
-
-    mysql_close(conn);
-}
-
-void main_account_management();
-void main_account_management(){
-    
-}
-
 int main() {
+<<<<<<< HEAD
     printf("compile done");
     printf("compile done");
     printf("compile done");
     printf("compile done");
+=======
+    printf("compile");
+>>>>>>> a11781b (Changed by Piyush)
     return 0;
 }
