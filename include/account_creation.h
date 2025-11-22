3c0a09b83910f797e002f65114ab0b2da8edb12f
Ayushjain1928 <siddhajain31@gmail.com>
2025-11-22 12:29:32 +0000

    changed by ayush

--- include/account_creation.h
+++ include/account_creation.h
@@ -1,6 +1,11 @@
 #ifndef account_creation_H
-
+#define account_creation_H
+#define MAX_ROWS 100
+#define MAX_LENGTH 100
 int user_menu();
-void mysql_query_excuter(const char* ,const char*);
-
+void mysql_query_excuter(const char *query,
+                         const char *database,
+                         int permission,
+                         char data[MAX_ROWS][20][MAX_LENGTH],
+                         int* rows_out);
 #endif
